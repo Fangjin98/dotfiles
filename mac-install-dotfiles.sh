1a542a5e4d21ee4305246b9a9216f308097ae5a9
Joey <fangjin98@mail.ustc.edu.cn>
2023-07-20 19:54:25 +0800

    update macos

--- mac-install-dotfiles.sh
+++ mac-install-dotfiles.sh
@@ -1,8 +1,12 @@
 mkdir -p $HOME/.config
 
 ln -s "$(PWD)/src/common/.vimrc" "$HOME/.vimrc"
-ln -s "$(PWD)/src/mac/.zshrc" "$HOME/.zshrc"
+ln -s "$(PWD)/src/common/starship.toml" "$HOME/.config/starship.toml"
+ln -s "$(PWD)/src/common/.vim" "$HOME/.vim"
+ln -s "$(PWD)/src/common/.tmux.conf" "$HOME/.tmux.conf"
 
+ln -s "$(PWD)/src/mac/.zshrc" "$HOME/.zshrc"
 ln -s "$(PWD)/src/mac/.config/yabai" "$HOME/.config/yabai"
 ln -s "$(PWD)/src/mac/.config/skhd" "$HOME/.config/skhd"
 ln -s "$(PWD)/src/mac/.config/sketchybar" "$HOME/.config/sketchybar"
+
