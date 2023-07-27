3f68e1e1884fef8fcc71d35a9dba7f77b69af133
joey <fangjin98@outlook.com>
2023-07-27 10:43:30 +0800

    add p4 tools functions

--- linux-install-dotfiles.sh
+++ linux-install-dotfiles.sh
@@ -4,8 +4,11 @@ ln -s "$(pwd)/src/common/.vim" "$HOME/.vim"
 ln -s "$(pwd)/src/common/.tmux.conf" "$HOME/.tmux.conf"
 ln -s "$(pwd)/src/common/starship.toml" "$HOME/.config/starship.toml"
 
+ln -s "$(pwd)/src/linux/p4-tools" "$HOME/p4-tools"
+
 ln -s "$(pwd)/src/linux/.zshrc" "$HOME/.zshrc"
 ln -s "$(pwd)/src/common/.config/zsh" "$HOME/.config/zsh"
-ln -s "$(pwd)/src/linux/.bashrc" "$HOME/.bashrc"
-ln -s "$(pwd)/src/linux/.config/bash" "$HOME/.config/bash"
-ln -s "$(pwd)/src/linux/p4-tools" "$HOME/p4-tools"
+# ln -s "$(pwd)/src/linux/.bashrc" "$HOME/.bashrc"
+# ln -s "$(pwd)/src/linux/.config/bash" "$HOME/.config/bash"
+
+
--- src/linux/.zshrc
+++ src/linux/.zshrc
@@ -8,5 +8,8 @@ source $HOME/.config/zsh/alias
 source $HOME/.config/zsh/functions
 source $HOME/.config/zsh/default
 
+source $HOME/p4-tools/set_sde.bash
+source $HOME/p4-tools/functions
+
 # Welcome message
 echo -ne "Hello, $NICKNAME! It's "; date '+%A, %B %-d %Y'
