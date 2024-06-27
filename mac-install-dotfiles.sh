7931970436e667ce74b3a2d230783802ddabf6f9
Joey <fangjin98@outlook.com>
2024-06-27 20:12:43 +0800

    remove p4 syntax

--- mac-install-dotfiles.sh
+++ mac-install-dotfiles.sh
@@ -1,8 +1,5 @@
 ln -s "$(PWD)/src/common/.vimrc" "$HOME/.vimrc"
-ln -s "$(PWD)/src/common/.vim" "$HOME/.vim"
 ln -s "$(PWD)/src/common/.tmux.conf" "$HOME/.tmux.conf"
 ln -s "$(PWD)/src/common/starship.toml" "$HOME/.config/starship.toml"
 
-# zsh config
-ln -s "$(PWD)/src/common/.config/zsh" "$HOME/.config/zsh"
 ln -s "$(PWD)/src/mac/.zshrc" "$HOME/.zshrc"
