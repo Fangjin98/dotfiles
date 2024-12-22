1e8b477db02a075da4450bead7ac9b309b157caa
Joey <fangjin98@outlook.com>
2024-12-22 22:24:11 +0800

    add joplin theme

--- mac-install-dotfiles.sh
+++ mac-install-dotfiles.sh
@@ -2,5 +2,7 @@ ln -s "$(PWD)/src/common/.vimrc" "$HOME/.vimrc"
 ln -s "$(PWD)/src/common/nvim" "$HOME/.config/nvim"
 ln -s "$(PWD)/src/common/.tmux.conf" "$HOME/.tmux.conf"
 ln -s "$(PWD)/src/common/starship.toml" "$HOME/.config/starship.toml"
+ln -s "$(PWD)/src/common/joplin/userchrome.css" "$HOME/.config/joplin-desktop/userchrome.css"
+ln -s "$(PWD)/src/common/joplin/userstyle.css" "$HOME/.config/joplin-desktop/userstyle.css"
 
 ln -s "$(PWD)/src/mac/.zshrc" "$HOME/.zshrc"
