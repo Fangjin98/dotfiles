80c81ba152391caa071fd662b9684a2d7ace1e28
Joey <fangjin98@outlook.com>
2024-12-22 16:33:25 +0800

    add nvim config

--- mac-install-dotfiles.sh
+++ mac-install-dotfiles.sh
@@ -1,4 +1,5 @@
 ln -s "$(PWD)/src/common/.vimrc" "$HOME/.vimrc"
+ln -s "$(PWD)/src/common/nvim" "$HOME/.config/nvim"
 ln -s "$(PWD)/src/common/.tmux.conf" "$HOME/.tmux.conf"
 ln -s "$(PWD)/src/common/starship.toml" "$HOME/.config/starship.toml"
 
